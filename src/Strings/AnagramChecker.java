d7af0ec70aa5f081f4f160eb732f08aa00fb84b9
dvstharun <2457129@cognizant.com>
2026-04-22 11:33:13 +0530

    solved one string problem

---
+++ src/Strings/AnagramChecker.java
@@ -0,0 +1,37 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class AnagramChecker {
+
+
+    public static boolean isAnagram(String str1, String str2) {
+
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        String word1 = "listen";
+        String word2 = "silent";
+
+        if (isAnagram(word1, word2)) {
+            System.out.println(word1 + " and " + word2 + " are anagrams.");
+        } else {
+            System.out.println(word1 + " and " + word2 + " are NOT anagrams.");
+        }
+    }
+}
